718189b6a6508187246ad195c3c08cc125314560
Shahoodzee <shahood.bin.amir@gmail.com>
2026-06-13 05:41:59 +0500

    Initialize properties in JsonSmithAISettings and SupabaseSettings classes with default empty strings.

--- JsonSmith/Models/JsonSmithAISettings.cs
+++ JsonSmith/Models/JsonSmithAISettings.cs
@@ -2,7 +2,7 @@ namespace JsonSmith.Models
 {
     public class JsonSmithAISettings
     {
-        public string BASE_URL { get; set; }
-        public string API_KEY { get; set; }
+        public string BASE_URL { get; set; } = "";
+        public string API_KEY { get; set; } = "";
     }
 }
